6f0e6848e1f65f2b5790ac23e775aeceea87e006
very-random-gamer <jus.wang404@gmail.com>
2026-01-15 18:09:07 -0800

    WarmUp (String Alg)

---
+++ II - Practical Algorithms/String Algorithms/WarmUp.java
@@ -0,0 +1,12 @@
+public class WarmUp {
+    public static void main(String[] args) {
+        int min = 5;
+        int max = 10;
+
+        // write code that returns the square of a random integer between min and max (inclusive)
+        /* reminders:
+            (int) (Math.random() * (max - min + 1) + 1) -- Returns random integer
+            Math.pow(int a, int b) -- returns double value of a^b
+         */
+    }
+}
